1d14fd386ca4f91592f2b38fe2218ce7b480ceb9
zjad <108188072+tobelogin@users.noreply.github.com>
2026-02-24 00:25:47 +0800

    Refector for less mem consumption

--- src/main/cpp/av-helper/av-helper.c
+++ src/main/cpp/av-helper/av-helper.c
@@ -1,5 +1,3 @@
-#include <jni.h>
-#include <android/log.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,61 +7,14 @@
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
 
-#include "frame_queue.h"
+typedef struct {
+    AVFormatContext *in_ctx, *out_ctx;
+    AVCodecContext *dec_ctx, *enc_ctx;
+    struct SwsContext *sws_ctx;
+} LibavContext;
 
-// TODO 保留原队列中的内容，或者直接对原队列中的帧进行操作
-int color_converse_batch(FrameQueue *origin, FrameQueue *dst, enum AVPixelFormat dst_fmt)
+int init_in_ctx(AVFormatContext **in_ctx, const char *list_path)
 {
-    AVFrame *ori_frame = framequeue_pop(origin);
-    AVFrame *tmp_frame = av_frame_alloc();
-    if (tmp_frame == NULL)
-    {
-        fprintf(stderr, "Unable to alloc tmporary frame.\n");
-        return -1;
-    }
-    int width = ori_frame->width, height = ori_frame->height;
-    enum AVPixelFormat format = ori_frame->format;
-    struct SwsContext *context = sws_getContext(width, height, format,
-        width, height, dst_fmt, SWS_BICUBIC, NULL, NULL, NULL);
-    if (context == NULL)
-    {
-        fprintf(stderr,
-            "Error when allocating sws context.\n");
-        exit(1);
-    }
-
-    int ret = sws_scale_frame(context, tmp_frame, ori_frame);
-    while (ret >= 0)
-    {
-        tmp_frame->time_base = ori_frame->time_base;
-        tmp_frame->duration = ori_frame->duration;
-        tmp_frame->pts = ori_frame->pts;
-        framequeue_add(dst, tmp_frame);
-        tmp_frame = ori_frame;
-        av_frame_unref(tmp_frame);
-        ori_frame = framequeue_pop(origin);
-        if (ori_frame == NULL)
-        {
-            break;
-        }
-        ret = sws_scale_frame(context, tmp_frame, ori_frame);
-    }
-    if (ret < 0)
-    {
-        fprintf(stderr,
-            "Error when conversing pixel format: %s.\n", av_err2str(ret));
-        return -1;
-    }
-    av_frame_free(&tmp_frame);
-
-    sws_freeContext(context);
-    return 0;
-}
-
-// 解码 concat 获取到的内容
-int get_frames_from_list(const char *list_path, FrameQueue *queue)
-{
-    AVFormatContext *in_ctx = NULL;
     const AVInputFormat *in_fmt = av_find_input_format("concat");
     int ret=0;
     if (in_fmt == NULL)
@@ -73,14 +24,14 @@ int get_frames_from_list(const char *list_path, FrameQueue *queue)
     }
 
     AVDictionary *in_options = NULL;
-    ret = av_dict_set(&in_options, "safe", "0", 0); // 相当于命令行选项 -update 1
+    ret = av_dict_set(&in_options, "safe", "0", 0); // 相当于命令行选项 -safe 0
     if (ret < 0)
     {
         fprintf(stderr, "Failed to set input context options!\n");
         return -1;
     }
 
-    ret = avformat_open_input(&in_ctx, list_path, in_fmt, &in_options);
+    ret = avformat_open_input(in_ctx, list_path, in_fmt, &in_options);
     if (ret < 0)
     {
         fprintf(stderr, "Failed to open list file!\n");
@@ -88,7 +39,46 @@ int get_frames_from_list(const char *list_path, FrameQueue *queue)
     }
 
     av_dict_free(&in_options);
+    return 0;
+}
 
+int init_out_ctx(AVFormatContext **out_ctx,
+    const AVCodecContext *enc_ctx, const char *webp_path)
+{
+    const AVOutputFormat *output_format = av_guess_format("image2", webp_path, NULL);
+    if(avformat_alloc_output_context2(out_ctx, output_format, "image2", webp_path) < 0)
+    {
+        fprintf(stderr, "Unable to open output file\n");
+        return -1;
+    }
+
+    AVStream *out_stream = avformat_new_stream(*out_ctx, NULL); // 第二个参数未使用
+    if (!out_stream)
+    {
+        fprintf(stderr, "Could not allocate stream\n");
+        return 1;
+    }
+    int ret = avcodec_parameters_from_context(out_stream->codecpar, enc_ctx);
+    if (ret < 0)
+    {
+        fprintf(stderr, "Could not set stream info\n");
+        return 1;
+    }
+
+    AVDictionary *out_options = NULL;
+    ret = av_dict_set(&out_options, "update", "1", 0); // 相当于命令行选项 -update 1
+    (ret >= 0) && (ret = avformat_init_output(*out_ctx, &out_options));
+    if (ret < 0) {
+        fprintf(stderr, "Could not set output context options.\n");
+        return -1;
+    }
+    av_dict_free(&out_options);
+
+    return 0;
+}
+
+int init_dec_ctx(AVCodecContext **dec_ctx, AVFormatContext *in_ctx)
+{
     const AVCodec *decoder=avcodec_find_decoder(in_ctx->streams[0]->codecpar->codec_id); // 需要选对解码器
     AVCodecContext *dec_context = avcodec_alloc_context3(decoder); // 选对了解码器 libavcodec 才能猜测到视频流的像素格式
     if (!dec_context) {
@@ -101,254 +91,212 @@ int get_frames_from_list(const char *list_path, FrameQueue *queue)
         exit(1);
     }
     if(avcodec_parameters_to_context(dec_context,
-        in_ctx->streams[0]->codecpar) < 0) // 才能将解码器需要的信息读取出来
+                                     in_ctx->streams[0]->codecpar) < 0) // 才能将解码器需要的信息读取出来
     {
         fprintf(stderr, "Error when filling dec_context.\n");
         exit(1);
     }
-    ret = avcodec_open2(dec_context, decoder, NULL);
+    int ret = avcodec_open2(dec_context, decoder, NULL);
     if (ret < 0) {
         fprintf(stderr,
-            "Could not open video codec: %s\n", av_err2str(ret));
+                "Could not open video codec: %s\n", av_err2str(ret));
         exit(1);
     }
 
-    AVPacket *packet=av_packet_alloc();
-    if (packet == NULL)
+    *dec_ctx = dec_context;
+    return 0;
+}
+
+int init_enc_ctx(AVCodecContext **enc_ctx, const AVFormatContext *in_ctx,
+        enum AVCodecID codec_id, enum AVPixelFormat pix_fmt)
+{
+    if (in_ctx->streams[0]->codecpar->codec_type != AVMEDIA_TYPE_VIDEO)
     {
-        fprintf(stderr, "Unable to allocate packet.\n");
+        fprintf(stderr, "Stream 0 is not a video stream.\n");
+        return 1;
+    }
+    const AVCodec *encoder = avcodec_find_encoder(codec_id);
+    if (!encoder)
+    {
+        fprintf(stderr, "Could not find encoder for '%s'\n",
+                avcodec_get_name(codec_id));
+        return 1;
+    }
+    *enc_ctx = avcodec_alloc_context3(encoder);
+    if (!enc_ctx) {
+        fprintf(stderr, "Could not alloc an encoding context\n");
+        return 1;
+    }
+    AVCodecContext *enc_context = *enc_ctx;
+    enc_context->codec_id = codec_id;
+    enc_context->width = in_ctx->streams[0]->codecpar->width;
+    enc_context->height = in_ctx->streams[0]->codecpar->height;
+    enc_context->pix_fmt = pix_fmt;
+    enc_context->time_base = in_ctx->streams[0]->time_base;
+
+    int ret = avcodec_open2(*enc_ctx, encoder, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Could not open video codec: %s\n", av_err2str(ret));
         return -1;
     }
-    ret = av_read_frame(in_ctx, packet);
+
+    return 0;
+}
+
+int init_sws_ctx(struct SwsContext **sws_ctx, const AVCodecContext *enc_ctx,
+    const AVCodecContext *dec_ctx)
+{
+    *sws_ctx = sws_getContext(
+        dec_ctx->width, dec_ctx->height, dec_ctx->pix_fmt,
+        enc_ctx->width, enc_ctx->height, enc_ctx->pix_fmt,
+        SWS_BICUBIC, NULL, NULL, NULL);
+    if (*sws_ctx == NULL)
+    {
+        fprintf(stderr,
+            "Error when allocating sws context.\n");
+        return 1;
+    }
+    return 0;
+}
+
+int list2webp(const char *list_path, const char *webp_path)
+{
+    LibavContext av_ctx;
+    memset(&av_ctx, 0, sizeof(av_ctx));
+    int ret = 0;
+    ret = init_in_ctx(&av_ctx.in_ctx, list_path);
+    (ret >= 0) && (ret = init_dec_ctx(&av_ctx.dec_ctx, av_ctx.in_ctx));
+    (ret >= 0) && (ret = init_enc_ctx(&av_ctx.enc_ctx, av_ctx.in_ctx,
+        AV_CODEC_ID_WEBP, AV_PIX_FMT_YUV420P));
+    (ret >= 0) && (ret = init_out_ctx(&av_ctx.out_ctx, av_ctx.enc_ctx, webp_path));
+    (ret >= 0) && (ret = init_sws_ctx(&av_ctx.sws_ctx, av_ctx.enc_ctx, av_ctx.dec_ctx));
+    if (ret < 0)
+    {
+        fprintf(stderr, "Initialize failed!\n");
+        return -1;
+    }
+
+
+    AVPacket *in_packet=av_packet_alloc();
+    AVPacket *out_packet=av_packet_alloc();
+    if (in_packet == NULL || out_packet == NULL)
+    {
+        fprintf(stderr, "Unable to allocate packets.\n");
+        av_packet_free(&in_packet);
+        av_packet_free(&out_packet);
+        return -1;
+    }
+    AVFrame *in_frame = av_frame_alloc();
+    AVFrame *out_frame = av_frame_alloc();
+    if (in_frame == NULL || out_frame == NULL)
+    {
+        fprintf(stderr, "Unable to allocate frames.\n");
+        av_frame_free(&in_frame);
+        av_frame_free(&out_frame);
+        return -1;
+    }
+
+    ret = avformat_write_header(av_ctx.out_ctx, NULL);
+    if (ret < 0)
+    {
+        fprintf(stderr, "Error when writing header.\n"); // TODO 释放内存
+        return 1;
+    }
+
+    ret = av_read_frame(av_ctx.in_ctx, in_packet);
+    // decode and encode a frame
     while (ret >= 0)
     {
-        AVFrame *dec_frame = av_frame_alloc();
-        if (dec_frame == NULL)
-        {
-            fprintf(stderr, "Unable to allocate frame for decoding.\n");
-            return -1;
-        }
-        ret = avcodec_send_packet(dec_context, packet);
+        ret = avcodec_send_packet(av_ctx.dec_ctx, in_packet);
         if(ret < 0)
         {
             fprintf(stderr,
                 "Error when sending packet: %s.\n", av_err2str(ret));
             return -1;
         }
-        av_packet_unref(packet);
-        ret = avcodec_receive_frame(dec_context, dec_frame);
+        ret = avcodec_receive_frame(av_ctx.dec_ctx, in_frame);
         if (ret < 0)
         {
             fprintf(stderr,
                 "Error when recving frame: %s.\n", av_err2str(ret));
             return -1;
         }
-        dec_frame->time_base = in_ctx->streams[0]->time_base;
-        dec_frame->duration = in_ctx->streams[0]->duration;
-        framequeue_add(queue, dec_frame);
-        ret = av_read_frame(in_ctx, packet);
-    }
-    if(ret != AVERROR_EOF)
-    {
-        fprintf(stderr, "Unable to read picture\n");
-        avformat_close_input(&in_ctx);
-        return -1;
-    }
-    avcodec_send_packet(dec_context, NULL);
-    av_packet_free(&packet);
 
-    avcodec_free_context(&dec_context);
-    avformat_close_input(&in_ctx);
-    return 0;
-}
-
-int frames2webp(FrameQueue *queue, const char *out_path)
-{
-    AVFormatContext *output_context = NULL;
-
-    const AVOutputFormat *output_format = av_guess_format("image2", out_path, NULL);
-    if(avformat_alloc_output_context2(&output_context, output_format, "image2", out_path) < 0)
-    {
-        fprintf(stderr, "Unable to open output file\n");
-        return -1;
-    }
-    const AVCodec *encoder = avcodec_find_encoder(AV_CODEC_ID_WEBP);
-    if(!encoder)
-    {
-        fprintf(stderr, "Could not find encoder for '%s'\n",
-                avcodec_get_name(output_format->video_codec));
-        return 1;
-    }
-    AVCodecContext *enc_context = avcodec_alloc_context3(encoder);
-    if (!enc_context) {
-        fprintf(stderr, "Could not alloc an encoding context\n");
-        return 1;
-    }
-
-    AVFrame *frame = framequeue_pop(queue);
-
-    enc_context->codec_id = AV_CODEC_ID_WEBP;
-    enc_context->width = frame->width;
-    enc_context->height = frame->height;
-    enc_context->time_base = frame->time_base;
-    enc_context->pix_fmt = frame->format;
-
-    int ret = avcodec_open2(enc_context, encoder, NULL);
-    if (ret < 0) {
-        fprintf(stderr, "Could not open video codec: %s\n", av_err2str(ret));
-        return -1;
-    }
-
-    AVStream *output_stream = avformat_new_stream(output_context, NULL); // 第二个参数未使用
-    if (!output_stream)
-    {
-        fprintf(stderr, "Could not allocate stream\n");
-        exit(1);
-    }
-    if (avcodec_parameters_from_context(output_stream->codecpar, enc_context) < 0)
-    {
-        fprintf(stderr, "Could not set stream info\n");
-        exit(1);
-    }
-    output_stream->id=output_context->nb_streams-1;
-    output_stream->duration=frame->duration;
-    // output_stream->nb_frames=1;
-
-    while (frame != NULL && ret >= 0)
-    {
-        ret = avcodec_send_frame(enc_context, frame); // 这里似乎后来的帧会覆盖之前的帧
-        av_frame_free(&frame);
-        frame = framequeue_pop(queue);
-    }
-    if (ret < 0)
-    {
-        fprintf(stderr, "Unable to reencode frames.\n");
-        return -1;
-    }
-    avcodec_send_frame(enc_context, NULL);
-
-    AVPacket *packet = av_packet_alloc();
-    if(packet == NULL)
-    {
-        fprintf(stderr, "Failed to allocate packet!\n");
-        return -1;
-    }
-
-    AVDictionary *out_options = NULL;
-    ret = av_dict_set(&out_options, "update", "1", 0); // 相当于命令行选项 -update 1
-    (ret >= 0) && (ret = avformat_init_output(output_context, &out_options));
-    if (ret < 0) {
-        fprintf(stderr, "Could not set output context options.\n");
-        return -1;
-    }
-    av_dict_free(&out_options);
-
-    ret = avformat_write_header(output_context, NULL);
-    if(ret < 0)
-    {
-        fprintf(stderr, "Error when writing header.\n");
-        av_packet_unref(packet);
-        av_packet_free(&packet);
-        avformat_free_context(output_context);
-        return 1;
-    }
-    ret = avcodec_receive_packet(enc_context, packet);
-    while (ret >= 0)
-    {
-        ret = av_write_frame(output_context, packet);
+        // color conversion
+        // out_frame->format = av_ctx.enc_ctx->pix_fmt;
+        ret = sws_scale_frame(av_ctx.sws_ctx, out_frame, in_frame);
         if (ret < 0)
         {
-            fprintf(stderr, "Unable to write packet.\n");
+            fprintf(stderr,
+                "Error when conversing pixel format: %s.\n", av_err2str(ret));
             return -1;
         }
-        av_packet_unref(packet);
-        ret = avcodec_receive_packet(enc_context, packet);
+
+        // encode a frame
+        out_frame->time_base = in_frame->time_base;
+        // out_frame->duration = av_ctx.in_ctx->streams[0]->duration;
+        out_frame->pts = in_frame->pts;
+        ret = avcodec_send_frame(av_ctx.enc_ctx, out_frame);
+        if (ret < 0)
+        {
+            fprintf(stderr,
+                "Error when sending frame to encoder: %s.\n", av_err2str(ret));
+            return -1;
+        }
+        av_packet_unref(in_packet);
+        av_frame_unref(in_frame);
+        av_frame_unref(out_frame);
+
+        ret = av_read_frame(av_ctx.in_ctx, in_packet);
     }
     if (ret != AVERROR_EOF)
     {
-        fprintf(stderr, "Unable to read frames from decoder.\n");
+        fprintf(stderr, "Unable to read input packet.\n");
     }
-    av_write_frame(output_context, NULL);
-
-    av_packet_free(&packet);
-    avformat_free_context(output_context);
-    return 0;
-}
-
-int list2webp(const char *list_path, const char *out_img)
-{
-    FrameQueue *ori_frames = framequeue_alloc();
-    int ret = 0;
-    if(ori_frames == NULL)
+    else
     {
+        ret = 0;
+    }
+    avcodec_send_packet(av_ctx.dec_ctx, NULL);
+    avcodec_send_frame(av_ctx.enc_ctx, NULL);
+
+    // 必须要先 send NULL 才能 receive packet
+    // 但其实下面的循环也只执行了一次
+    ret = avcodec_receive_packet(av_ctx.enc_ctx, out_packet);
+    while (ret == 0)
+    {
+        ret = av_write_frame(av_ctx.out_ctx, out_packet);
+        if (ret < 0)
+        {
+            fprintf(stderr, "Unable to write packet: %s.\n", av_err2str(ret));
+            return -1;
+        }
+        av_packet_unref(out_packet);
+        ret = avcodec_receive_packet(av_ctx.enc_ctx, out_packet);
+    }
+    if (ret != AVERROR_EOF)
+    {
+        fprintf(stderr, "Unable to read packet from encoder: %s.\n", av_err2str(ret));
         return -1;
     }
-    ret = get_frames_from_list(list_path, ori_frames);
-    if (ret < 0)
+    else
     {
-        __android_log_print(ANDROID_LOG_ERROR, "nativeList2webp", "Error when reading input frames.\n");
-        return -1;
+        ret = 0;
     }
 
-    FrameQueue *yuv_frames = framequeue_alloc();
-    if(yuv_frames == NULL)
-    {
-        return -1;
-    }
-    ret = color_converse_batch(ori_frames, yuv_frames, AV_PIX_FMT_YUV420P);
-    if (ret < 0)
-    {
-        __android_log_print(ANDROID_LOG_ERROR, "nativeList2webp", "Error when conversing pixel format.\n");
-        return -1;
-    }
-    framequeue_free(ori_frames);
-    ori_frames = NULL;
+    av_write_frame(av_ctx.out_ctx, NULL);
+    av_write_trailer(av_ctx.out_ctx);
 
-    ret = frames2webp(yuv_frames, out_img);
-    if (ret < 0)
-    {
-        __android_log_print(ANDROID_LOG_ERROR, "nativeList2webp", "Error when encoding frames.\n");
-        return -1;
-    }
-    framequeue_free(yuv_frames);
+    av_packet_free(&in_packet);
+    av_frame_free(&in_frame);
+    av_frame_free(&out_frame);
+    av_packet_free(&out_packet);
 
-    return 0;
-}
+    sws_freeContext(av_ctx.sws_ctx);
+    avcodec_free_context(&av_ctx.dec_ctx);
+    avformat_close_input(&av_ctx.in_ctx);
+    avcodec_free_context(&av_ctx.enc_ctx);
+    avformat_free_context(av_ctx.out_ctx);
 
-JNIEXPORT jint JNICALL
-Java_io_github_tobelogin_FormatConverter_00024Companion_nativeList2Webp(JNIEnv *env, jobject thiz,
-                                                                        jbyteArray list_path,
-                                                                        jbyteArray webp_path)
-{
-    jbyte *tmp=(*env)->GetByteArrayElements(env, list_path, NULL);
-    jsize len=(*env)->GetArrayLength(env, list_path);
-    char *list_path_utf=malloc(len+1);
-    if (list_path_utf == NULL)
-    {
-        __android_log_print(ANDROID_LOG_DEBUG, "nativeList2webp", "Unable to allocate list file path buffer.\n");
-        (*env)->ReleaseByteArrayElements(env, list_path, tmp, JNI_ABORT);
-        return -1;
-    }
-    strncpy(list_path_utf, (const char *)tmp, len);
-    list_path_utf[len]=0; // 如果 src 更长则 strncpy 不会自动加 null
-    (*env)->ReleaseByteArrayElements(env, list_path, tmp, JNI_ABORT);
-
-    tmp=(*env)->GetByteArrayElements(env, webp_path, NULL);
-    len=(*env)->GetArrayLength(env, webp_path);
-    char *webp_path_utf=malloc(len+1);
-    if (webp_path_utf == NULL)
-    {
-        __android_log_print(ANDROID_LOG_DEBUG, "nativeList2webp", "Unable to allocate list file path buffer.\n");
-        (*env)->ReleaseByteArrayElements(env, webp_path, tmp, JNI_ABORT);
-        return -1;
-    }
-    strncpy(webp_path_utf, (const char *)tmp, len);
-    webp_path_utf[len]=0;
-    (*env)->ReleaseByteArrayElements(env, webp_path, tmp, JNI_ABORT);
-
-    __android_log_print(ANDROID_LOG_DEBUG, "nativeList2webp", "List path is %s, webp path is %s\n", list_path_utf, webp_path_utf);
-    int ret = list2webp((const char*)list_path_utf, (const char*)webp_path_utf);
-    free(list_path_utf);
-    free(webp_path_utf);
-    return ret;
+    return ret >= 0 ? 0 : ret;
 }
